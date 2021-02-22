43336ff204a6b26e5b7097b0c9e192042e9ca342
schelakanthan <you@example.com>
2021-02-22 16:49:10 +0530

    musicplayer

--- main.js
+++ main.js
@@ -4,13 +4,178 @@ SC.initialize({
 
 $(document).ready(function() { 
     
-  SC.stream('/tracks/261780815',function(sound){
+  SC.stream('/tracks/286577810',function(sound){
         $('#start').click(function(e) {
             e.preventDefault();
             sound.start();
           });
 
           $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });  
+     });  
+ });
+
+ $(document).ready(function() { 
+    
+  SC.stream('/tracks/191214159',function(sound){
+        $('#start1').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop1').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });  
+     });  
+ });
+
+ $(document).ready(function() { 
+    
+  SC.stream('/tracks/652012811',function(sound){
+        $('#start2').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop2').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });  
+     });  
+ });
+
+ $(document).ready(function() { 
+    
+  SC.stream('/tracks/225519099',function(sound){
+        $('#start3').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop3').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });  
+     });  
+ });
+
+ $(document).ready(function() { 
+    
+  SC.stream('/tracks/496210638',function(sound){
+        $('#start4').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop4').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });  
+     });  
+ });
+
+ $(document).ready(function() { 
+    
+  SC.stream('/tracks/112010796',function(sound){
+        $('#start5').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop5').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });  
+     });  
+ });
+
+ $(document).ready(function() { 
+    
+  SC.stream('/tracks/976628071',function(sound){
+        $('#start6').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop6').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });  
+     });  
+ });
+
+ $(document).ready(function() { 
+    
+  SC.stream('/playlists/236267079',function(sound){
+        $('#start7').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop7').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });  
+     });  
+ });
+
+ $(document).ready(function() { 
+    
+  SC.stream('/tracks/456435057',function(sound){
+        $('#start8').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop8').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });  
+     });  
+ });
+
+ $(document).ready(function() { 
+    
+  SC.stream('/tracks/668644226',function(sound){
+        $('#start9').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop9').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });  
+     });  
+ });
+
+ $(document).ready(function() { 
+    
+  SC.stream('/tracks/524553387',function(sound){
+        $('#start10').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop10').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+          });  
+     });  
+ });
+
+ $(document).ready(function() { 
+    
+  SC.stream('/tracks/319583561',function(sound){
+        $('#start11').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop11').click(function(e) {
             e.preventDefault();
             sound.stop();
           });  
